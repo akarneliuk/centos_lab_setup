fc6dac78c177f2d3a92a5cc20c33b1a37a1f3977
akarneliuk <a.karneliuk@gmail.com>
2019-05-10 23:15:35 +0200

    0.2

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ I'm quite often rebuilding the lab VM or server and to ease this task decided to
 - Installation of Ansible and all dependencies
 - Installation of useful Python packages (pyang, ncclient, ipaddr, etc)
 - Installation of Virtual Box and all dependencies
+- Installation of KVM and all dependencies
 - Installation of Vagrant and all dependencies
 
 ## How to use
-Just download or copy the content of the files on the freshly installed CentOS Linux and push `./initial_setup.sh`
+Just download or copy the content of the files on the freshly installed CentOS Linux and execute:
+- `./initial_setup_virtualbox.sh` for installation with Virtual Box
+- `./initial_setuo_kvm.sh` for installation with QEMU/KVM
---
+++ initial_setup_kvm.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+echo 'INSTALLING AND UPDATING APPS // YUM ...'
+
+sudo yum -y update && sudo yum -y upgrade
+sudo yum -y install ansible python python-devel epel-release python-pip vim gcc git python-netaddr wget tcpdump curl vconfig
+sudo yum -y group install "Development Tools"
+
+echo 'INSTALLING AND UPDATING APPS // PIP ...'
+
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+sudo pip install --upgrade lxml
+sudo pip install -r requirements.txt
+
+echo 'INSTALLING AND UPDATING APPS // KVM ...'
+
+sudo yum -y install qemu-kvm qemu-img virt-manager libvirt libvirt-python libvirt-client virt-install virt-viewer
+
+echo 'INSTALLING AND UPDATING APPS // VAGRANT ...'
+
+sudo wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm
+sudo yum -y localinstall vagrant_2.2.4_x86_64.rpm
+echo '=============================================================================================' > installation.log
+echo 'vagrant --version'
+vagrant --version >> installation.log
+echo '=============================================================================================' >> installation.log
+
+echo 'CREATING BRIDGE FOR OOB ...'
+echo 'DEVICE=br0' > ifcfg-br0
+echo 'TYPE=Bridge' >> ifcfg-br0
+echo 'BOOTPROTO=none' >> ifcfg-br0
+echo 'ONBOOT=yes' >> ifcfg-br0
+echo 'IPADDR=192.168.1.1' >> ifcfg-br0
+echo 'PREFIX=24' >> ifcfg-br0
+echo 'DELAY=0' >> ifcfg-br0
+echo 'IPV6INIT=yes' >> ifcfg-br0
+echo 'IPV6FORWARDING=yes' >> ifcfg-br0
+echo 'IPV6_AUTOCONF=no' >> ifcfg-br0
+echo 'IPV6_FAILURE_FATAL=no' >> ifcfg-br0
+echo 'ZONE=public' >> ifcfg-br0
+echo 'IPV6ADDR=fc00:de:1:ffff::1/64' >> ifcfg-br0
+sudo mv ifcfg-br0 /etc/sysconfig/network-scripts/
+sudo systemctl restart network
+echo '=============================================================================================' > installation.log
+echo 'ip addr show dev br0'
+ip addr show dev br0 >> installation.log
+echo '=============================================================================================' >> installation.log
+
+echo 'WATCHING LOGS ...'
+cat installation.log
+
+echo 'CLEARING TEMPs ...'
+sudo rm installation.log
+sudo rm vagrant_2.2.4_x86_64.rpm 
